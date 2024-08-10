cb05283cf46bd3bdfc887144176360ff72e02664
anis2566 <anichuranis1000@gmail.com>
2024-08-10 16:09:09 +0600

    faaafaf

--- app/payment/success/page.tsx
+++ app/payment/success/page.tsx
@@ -1,38 +1,19 @@
-"use client"
+import { Suspense } from "react"
+import { Metadata } from "next"
 
-import { CircleCheck } from "lucide-react"
-import { useRouter, useSearchParams } from "next/navigation"
-import { useEffect } from "react"
+import { Loader } from "@/components/loader"
+import { SuccessCard } from "./_components/success-card"
 
-import { Card, CardContent } from "@/components/ui/card"
+export const metadata: Metadata = {
+  title: "APBn Scouts | Payment Failed",
+  description: "Apbn scouts group",
+}
 
 const PaymentSuccess = () => {
-
-  const callback = useSearchParams().get("callback")
-  const router = useRouter()
-
-  useEffect(() => {
-    if (callback) {
-      setTimeout(() => {
-        router.push(callback)
-      }, 5000)
-    }
-  }, [callback, router])
-
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <Card className="w-full max-w-md">
-        <CardContent className="space-y-6 p-6">
-          <div className="flex flex-col items-center justify-center">
-            <CircleCheck className="h-12 w-12 text-green-500" />
-            <h2 className="mt-4 text-2xl font-bold">Payment Successful</h2>
-            <p className="mt-2 text-muted-foreground text-center">
-              Congratulations! Your payment was processed successfully. You will redirect soon...
-            </p>
-          </div>
-        </CardContent>
-      </Card>
-    </div>
+    <Suspense fallback={<Loader />}>
+      <SuccessCard />
+    </Suspense>
   )
 }
 
